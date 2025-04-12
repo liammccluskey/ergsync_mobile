d57650514d5e918a5bb18379005a40764f73ba62
Liam Mccluskey <liammail100@gmail.com>
2025-04-12 13:02:06 -0400

    updated settings paghe

--- src/screens/Settings/index.js
+++ src/screens/Settings/index.js
@@ -58,8 +58,8 @@ const getStyles = theme => StyleSheet.create({
         fontSize: 15,
         color: theme.tint,
         padding: 20,
-        borderWidth: 1,
-        borderRadius: 5,
+        borderBottomWidth: 2,
+        borderRadius: 0,
         borderColor: theme.tint,
         paddingTop: 5,
         paddingBottom: 5
